775d48a7f717bedfbbda29568b577f311f573fa7
Rachel <rai.chel.ann@gmail.com>
2017-10-01 20:07:33 -0700

    removed commented out text in forms in trips

--- apps/trips/forms.py
+++ apps/trips/forms.py
@@ -5,17 +5,6 @@ import datetime
 from .models import Trip
 
 class NewTripForm(forms.ModelForm):
-    # destination = forms.CharField(label='Destination:', max_length=30)
-    # description = forms.CharField(
-    #     widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'What is on your mind?'}
-    #     ),
-    #     max_length=4000,
-    #     help_text='The max length of the text is 4000.'
-    # )
-    # start_date = forms.DateField(
-    #     input_formats='%m/%d/%Y')
-    # end_date = forms.DateField(
-    #     input_formats='%m/%d/%Y')
 
     class Meta:
         model = Trip
